852a5eae001f07a79ad1160a830903eb7dea953c
18bet <it.18bet1024@gmail.com>
2025-06-21 00:37:41 +0530

    Initial commit

---
+++ Models/candidate.js
@@ -0,0 +1,15 @@
+const mongoose = require('mongoose');
+//const bcrypt = require('bcrypt');
+
+const candidateSchema = new mongoose.Schema({
+    name:{
+        type :String,
+        required :true
+    },
+    
+});
+
+
+
+const Candidate = mongoose.model('Candidate',candidateSchema);
+module.exports =Candidate;
